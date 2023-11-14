49eb9b72327fee193632977d3415ac16978acef9
franba94 <132496171+franba94@users.noreply.github.com>
2023-11-14 15:59:57 -0300

    Add files via upload
    asd

---
+++ has.hs
@@ -0,0 +1,151 @@
+longitud :: [Int] -> Int 
+longitud [] = 0 
+longitud (x:xs) = 1 + longitud xs 
+
+
+ultimo :: [Int] -> Int
+ultimo [x] = x 
+ultimo (x:xs) = ultimo xs 
+
+principio :: [Int] -> [Int]
+principio [x] = [] 
+principio (x:xs) = x : (principio xs) 
+
+reverso :: [Int] -> [Int] 
+reverso [] = []
+reverso (x:xs) = ultimo (x:xs) : reverso(principio (x:xs)) 
+
+reverso2 :: (Eq t) => [t] -> [t]
+reverso2 (x:xs) | xs == [] = [x]
+                | otherwise = reverso2 xs ++ [x]
+
+pertenece :: Int -> [Int] -> Bool
+pertenece n [] = False
+pertenece n (x:xs) | n == x = True 
+                   | otherwise = pertenece n xs 
+
+todosIguales :: [Int] -> Bool
+todosIguales [x] = True
+todosIguales (x:y:ys) = x == y && todosIguales ys 
+
+todosDistintos :: [Int] -> Bool
+todosDistintos [] = True
+todosDistintos [x] = True
+todosDistintos (x:y:ys) | x /= y = todosDistintos (x:ys)
+                        | otherwise = False 
+
+
+hayRepetidos :: [Int] -> Bool
+hayRepetidos [] = False
+hayRepetidos (x:xs) | pertenece x xs = True
+                    | otherwise = hayRepetidos xs 
+
+
+
+quitar :: Int -> [Int] -> [Int]
+quitar _ [] = []
+quitar n (x:xs) | n == x = xs
+                | otherwise = x : quitar n (x:xs) 
+
+
+quitarTodos :: Int -> [Int] -> [Int]
+quitarTodos _ [] = []
+quitarTodos n (x:xs) | n == x = quitarTodos n xs
+                     | otherwise = x : quitarTodos n xs 
+
+
+eliminarRepetidos :: [Int] -> [Int]
+eliminarRepetidos [] = []
+eliminarRepetidos (x:xs) | hayRepetidos (x:xs) = x : (eliminarRepetidos (quitarTodos x (x:xs)))
+                         | otherwise = x : (eliminarRepetidos xs)
+
+
+perteneceA :: [Int] -> [Int] -> Bool
+perteneceA [] _ = True
+perteneceA (x:xs) (y:ys) | pertenece x (y:ys) && perteneceA xs (y:ys) = True
+                         | otherwise = False
+
+mismosElementos :: [Int] -> [Int] -> Bool 
+mismosElementos (x:xs) (y:ys) = perteneceA (x:xs) (y:ys) && perteneceA (y:ys) (x:xs) 
+
+capicua :: [Int] -> Bool
+capicua [_] = True
+capicua [] = True
+capicua (x:xs) | x == ultimo (x:xs) && capicua (principio xs) = True
+               | otherwise = False 
+
+
+sumatoria :: [Int] -> Int
+sumatoria [] = 0
+sumatoria (x:xs) = x + sumatoria xs 
+
+productoria :: [Int] -> Int
+productoria [] = 1 
+productoria (x:xs) = x * productoria xs 
+
+
+maximo :: [Int] -> Int
+maximo [x] = x 
+maximo (x:y:xs) | x >= y = maximo (x:xs)
+                | otherwise = maximo (y:xs)
+
+sumarN :: Int -> [Int] -> [Int]
+sumarN n [] = [] 
+sumarN n (x:xs) = (n + x) : sumarN n xs  
+
+
+sumarElPrimero :: [Int] -> [Int]
+sumarElPrimero [] = []
+sumarElPrimero (x:xs) = sumarN x (x:xs)
+
+
+sumarElUltimo :: [Int] -> [Int]
+sumarElUltimo (x:xs) = sumarN (ultimo (x:xs)) (x:xs)
+
+
+pares :: [Int] -> [Int]
+pares [] = []
+pares (x:xs) | mod x 2 == 0 = x : pares xs
+             | otherwise = pares xs 
+
+
+multiplosDeN :: Int -> [Int] -> [Int]
+multiplosDeN n [] = []
+multiplosDeN n (x:xs) | mod x n == 0 = x : multiplosDeN n xs 
+                      | otherwise = multiplosDeN n xs   
+
+ordenar :: [Int] -> [Int]
+ordenar [] = [] 
+ordenar (x:xs) = (minimo (x:xs)) : ordenar(quitarTodos (minimo(x:xs)) (x:xs))
+
+minimo :: [Int] -> Int
+minimo [x] = x 
+minimo (x:y:xs) | x <= y = minimo (x:xs)
+                | otherwise = minimo (y:xs)
+
+sumaAcumulada2 :: [Int] -> [Int]
+sumaAcumulada2 [] = []
+sumaAcumulada2 (x:xs) = sumatoria (x:xs) : sumaAcumulada2 (principio (x:xs))
+
+sumaAcumulada :: [Int] -> [Int]
+sumaAcumulada (x:xs) = reverso (sumaAcumulada2 (x:xs))
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
